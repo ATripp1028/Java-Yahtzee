11f0e8588aac4efa4b47eb640831a923702f2a60
ATripp1028 <alexhtripp@gmail.com>
2023-03-09 19:23:28 -0600

    Set up scanner

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         gameLoop();
     }
 
     public static void gameLoop() {
-
+        Scanner sc = new Scanner(System.in);
+        String input;
+        boolean running = true; 
+        sc.close();
     }
 }
