0655f1fd2e130dce257373f1f5d0b8712de0e771
ATripp1028 <alexhtripp@gmail.com>
2023-03-11 10:46:52 -0600

    Implemented support for the quit command

--- Main.java
+++ Main.java
@@ -1,14 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
+    boolean hasQuit = true;
     public static void main(String[] args) {
-        gameLoop();
+        new Main().gameLoop();
     }
 
-    public static void gameLoop() {
+    private void gameLoop() {
         Scanner sc = new Scanner(System.in);
         String input;
-        boolean running = true; 
+        boolean running = true;
+        boolean hasExecuted = false;
+        do {
+            do {
+                input = sc.nextLine();
+                hasExecuted = executeCommand(input);
+            } while (!hasExecuted);
+            running = (!hasQuit);
+        } while (running);
         sc.close();
+    }
+
+    private boolean executeCommand(String input) {
+        return (handleGameCommands(input) || handleSystemCommands(input));
+    }
+
+    private boolean handleGameCommands(String input) {
+        return true;
+    }
+
+    private boolean handleSystemCommands(String input) {
+        switch (input) {
+            case "q":
+            case "quit":
+                hasQuit = true;
+                return true;
+            default:
+                return false;
+        }
     }
 }
